e5fab6b1acf5ad4a899b88866cd1dc87fab4d8c3
Sai Prasad <saipm1@gmail.com>
2020-04-14 19:12:05 +0530

    commit

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class QuizQuestionButton extends Component {
   handleclick() {
-
+        this.props.clickHandler(this.props.button_text)
     }
   render() {
     return (
-      <li><button onClick = {this.handleclick.bind(this)} > {this.props.button_text}</button>
+      <li><button onClick = {this.handleclick.bind(this)}>{this.props.button_text}</button>
       </li>
     );
   }
